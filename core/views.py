3ff4bcf7ba9910518e483e90367e60030a73d5c0
Sheljin S.A <shelj73@gmail.com>
2025-06-04 21:05:33 +0530

    Update views.py

--- core/views.py
+++ core/views.py
@@ -14,7 +14,17 @@ class CatalogHomeView(APIView):
     Returns catalog main url.
     """
 
-
+    @extend_schema(
+        summary="Redirector",
+        description="Catalog main redirector",
+        request=None,
+        responses={
+            200: OpenApiResponse(
+                response=None,
+            )
+        },
+        tags=["Catalog"]
+    )
     def get(self, request):
         """
         Handles GET requests to return API documentation link dynamically.
